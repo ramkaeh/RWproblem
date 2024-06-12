236a965d921afaaef6ab923ffcd74a37559ce65f
ramkaeh <gmasiewicz@gmail.com>
2024-06-12 19:19:49 +0200

    secondComm

--- main2.c
+++ main2.c
@@ -1,50 +1,44 @@
-//zagodzenie pisarzy
 #include <pthread.h>
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
 
-// Semafory i zmienne globalne
-sem_t mutex;
-sem_t write_lock;
+// Lock i zmienne globalne
+pthread_mutex_t lock;
+pthread_cond_t okToRead; //Zmienna warunkowa do synchronizacji czytelnikow
+pthread_cond_t okToWrite; //Zmienna warunkowa do synchronizacji pisarzy
+int read_que=0;
 int read_count = 0;
-
 int write_count = 0;
-
-int sleepTime = 100000;
 int num_readers = 10;
 int num_writers = 4;
-    
+int sleepTime = 100000;
+
 void *reader(void *id) {
     int reader_id = *((int *)id);
     while (1) {
         usleep(sleepTime);  // Symulacja czasu między próbami wejścia do czytelni
         
-        
-        
-        sem_wait(&mutex); //Uzyskanie dostepu do sekcji krytycznej chronicej licznik czytelnikow
-        
-        read_count++;
-        
-        if (read_count == 1 ) {
-            sem_wait(&write_lock);// Pierwszy czytelnik blokuje dostep pisarzom
+        read_que++;
+        pthread_mutex_lock(&lock); //Zablokowanie mutexu
+        while (write_count > 0) {
+            pthread_cond_wait(&okToRead, &lock); //Czytelnik czeka na sygna wejscia
         }
-        sem_post(&mutex); //Zwolnienie semafora
+        read_que--;
+        read_count++;
+        pthread_mutex_unlock(&lock); //Odblokowanie mutexu
+
         makeMessage();
-        
         usleep(sleepTime);  // Symulacja czytania
         
-        
 
-        sem_wait(&mutex);
+        pthread_mutex_lock(&lock);
         read_count--;
         if (read_count == 0) {
-            sem_post(&write_lock); //Ostatni czytelnik odblokowuje dostep pisarzom
+            pthread_cond_signal(&okToWrite); // Sygnal, ze pisarz moze wejsc
         }
-        sem_post(&mutex);
-        
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
@@ -52,36 +46,44 @@ void *reader(void *id) {
 void *writer(void *id) {
     int writer_id = *((int *)id);
     while (1) {
-        usleep(2 * sleepTime);  // Symulacja czasu między próbami wejścia do czytelni
+        usleep(sleepTime);  // Symulacja czasu między próbami wejścia do czytelni
+        
+        
+        pthread_mutex_lock(&lock);
         
         
         
-
-        sem_wait(&write_lock); //Pisarz blokuje dostep do czytelni dla innych pisarzy i czytelnikow
-    
+        while (read_count > 0 || write_count > 0) {
         
+            pthread_cond_wait(&okToWrite, &lock); //Pisarz czeka na sygna wejscia
+        }
+        pthread_mutex_unlock(&lock);
         write_count++;
         makeMessage();
         
         usleep(sleepTime);  // Symulacja pisania
+        
+
+        pthread_mutex_lock(&lock);
         write_count--;
-        
-        
-        sem_post(&write_lock); //Pisarz odblokowuje dostep do czytelni
-        
+        if (write_count == 0) {
+            pthread_cond_broadcast(&okToRead); //Sygnal dla wszystkich oczekujacych czytelnikow, ze mozna wejsc
+        } else {
+            pthread_cond_signal(&okToWrite); //Sygnal dla innego pisarza
+        }
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
 //Tworzenie wiadomosci
 void makeMessage(){
-    printf("ReaderQ: %i WriterQ: %i [in: R: %i W: %i]\n",num_readers-read_count ,num_writers-write_count, read_count, write_count);
+    printf("ReaderQ: %i WriterQ: %i [in: R: %i W: %i]\n",read_que, num_writers-write_count, read_count, write_count);
 
 }
-
 int parseParameters(int argc, char **argv) // Funkcja wczytująca opcje programu: liczbę czytelników i pisarzy.
 {
     int option;
-    while ((option = getopt(argc, argv, ":R:W:")) != -1)
+    while ((option = getopt(argc, argv, ":R:W:")) != -1) 
     {
         switch (option)
         {
@@ -95,11 +97,11 @@ int parseParameters(int argc, char **argv) // Funkcja wczytująca opcje programu
             break;
         case ':':
             printf("opcja wymaga podania wartosci\n"); 
-            return -3; 
+            return -3;
             break;
         case '?':
             printf("nieznana opcja: %c\n", optopt); 
-            return -4; 
+            return -4;
             break;
         default:
             printf("blad"); 
@@ -113,13 +115,11 @@ int parseParameters(int argc, char **argv) // Funkcja wczytująca opcje programu
     return 0; 
 }
 
-
 int main(int argc, char *argv[]) {
-    if (parseParameters < 0 ){ //Wczytywanie opcji
-        
+    if (parseParameters(argc,argv)<0) { //Wczytywanie opcji
         return -1;
     }
-
+ 
     
 
     pthread_t readers[num_readers];
@@ -127,30 +127,31 @@ int main(int argc, char *argv[]) {
     int reader_ids[num_readers];
     int writer_ids[num_writers];
 
-    sem_init(&mutex, 0, 1);
-    sem_init(&write_lock, 0, 1);
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&okToRead, NULL);
+    pthread_cond_init(&okToWrite, NULL);
 
     for (int i = 0; i < num_readers; i++) { //Rozpoczynamy watki czytelnikow
         reader_ids[i] = i + 1;
         pthread_create(&readers[i], NULL, reader, &reader_ids[i]);
     }
-    sleep(2); //Czekamy 2 sekundy przed rozpoczeciem watkow pisarzy, aby opznic moment ich wejscia do czytelni
 
-    for (int i = 0; i < num_writers; i++) { //Rozpoczynamy watki pisarzy
-        writer_ids[i] = i + 1;
+    for (int i = 0; i < num_writers; i++) {//Rozpoczynamy watki pisarzy
+        writer_ids[i] = i + 1; 
         pthread_create(&writers[i], NULL, writer, &writer_ids[i]);
     }
 
     for (int i = 0; i < num_readers; i++) { //Czekamy glownym watkiem na zakonczenie watkow czytelnikow
-        pthread_join(readers[i], NULL); 
+        pthread_join(readers[i], NULL);
     }
 
     for (int i = 0; i < num_writers; i++) { //Czekamy glownym watkiem na zakonczenie watkow pisarzy
         pthread_join(writers[i], NULL);
     }
 
-    sem_destroy(&mutex);
-    sem_destroy(&write_lock);
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&okToRead);
+    pthread_cond_destroy(&okToWrite);
 
     return 0;
 }
